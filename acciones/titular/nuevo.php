635b3a8ef8dafea7e28abee7666ee966f153eb2c
AleVillalb4 <avillalba793@gmail.com>
2022-09-01 22:36:10 -0300

    finalizacion

--- acciones/titular/nuevo.php
+++ acciones/titular/nuevo.php
@@ -1,33 +1,33 @@
 <?php
-require_once '';
+
+header('Content-Type: application/json');
+
+require_once 'responses/nuevoResponse.php';
+require_once 'request/nuevoRequest.php';
+
 $json = file_get_contents('php://input', true);
 $req = json_decode($json);
 
 $resp = new NuevoResponse();
-
-$IsOk = true;
+$resp->IsOk = true;
 
 if ($req->Titular->Direccion == null) {
     $resp->IsOk = false;
     $resp->Mensaje = 'La direccion es obligatoria';
 }
+
 if ($req->Titular->NroDocumento == null) {
     $resp->IsOk = false;
-    $resp->Mensaje = 'El Apellido y Nombre es obligatorio';
+    $resp->Mensaje =$resp->Mensaje. '-El Numero de Documento es obligatorio';
 }
+
 if ($req->Titular->ApellidoNombre == null) {
     $resp->IsOk = false;
-    $resp->Mensaje = 'El Numero de Documento es obligatorio';
+    $resp->Mensaje =$resp->Mensaje. '-El Apellido y Nombre es obligatorio' ;
 } 
 
-if ($IsOk = true){ 
-    $resp->Mensaje = 'Titular agregado correctamente';
-}else if ($req->Titular->Direccion == null) {
-    $resp->IsOk = false;
-    $resp->Mensaje = 'La direccion es obligatoria-';
-}else if ($req->Titular->NroDocumento == null) {
-    $resp->IsOk = false;
-    $resp->Mensaje= $resp->Mensaje .'El Apellido y Nombre es obligatorio-';
-}else if ($req->Titular->ApellidoNombre == null){ 
-     $resp->Mensaje = $resp->Mensaje . '-El Numero de Documento es obligatorio';
-} 
+if ($resp->IsOk == true){ 
+    $resp->Mensaje = 'Titular agregado correctamente';    
+ }
+
+echo json_encode($resp);
